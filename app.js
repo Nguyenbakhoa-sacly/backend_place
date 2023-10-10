a2796b987793994ebc168576fc3eb87076ad6489
Nguyenbakhoa-sacly <khoana23062002@gmail.com>
2023-10-10 20:35:16 +0700

    createPlace, updatePlaceById, deletePlaceById

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 
 const placesRoutes = require('./routes/places-routes')
 
+const HttpError = require('./models/http-error')
+
 const app = express();
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -16,6 +18,10 @@ app.use(bodyParser.json());
 
 app.use('/api/places', placesRoutes)
 
+app.use((req, res, next) => {
+  const error = new HttpError('Could not find this route.', 404);
+  throw error;
+})
 app.use((err, req, res, next) => {
   // Kiểm tra xem tiêu đề phản hồi đã được gửi chưa
   if (res.headerSent) {
